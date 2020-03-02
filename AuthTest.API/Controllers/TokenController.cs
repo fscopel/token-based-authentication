c2eedc6f6a66a42d95ba85453b3dba7f81bf8d61
Fabio Scopel <fabiobscopel@gmail.com>
2020-03-01 22:36:59 -0600

    Initial commit

---
+++ AuthTest.API/Controllers/TokenController.cs
@@ -0,0 +1,27 @@
+﻿using AuthTest.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace AuthTest.API.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public TokenController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public string GetRandomToken()
+        {
+            var jwt = new JwtService(_config);
+            var token = jwt.GenerateSecurityToken("fake@email.com");
+            return token;
+        }
+    }
+}
---
+++ AuthTest.API/Middleware/AuthenticationMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace AuthTest.API.Middleware
+{
+    public static class AuthenticationExtension
+    {
+        public static IServiceCollection AddTokenAuthentication(this IServiceCollection services, IConfiguration config)
+        {
+            var secret = config.GetSection("JwtConfig").GetSection("secret").Value;
+
+            var key = Encoding.ASCII.GetBytes(secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                   // ValidIssuer = "localhost",
+                    //ValidAudience = "localhost"
+                };
+            });
+
+            return services;
+        }
+    }
+}
